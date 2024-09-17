44357e8604da82fee0b709afe6dc73b37256d1b2
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-09-17 11:58:47 -0300

    react native

--- app/perfil/index.jsx
+++ app/perfil/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image, Pressable } from 'react-native';
 import Header from './components/header';
-import { Link } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 const App = () => {
+    const router = useRouter();
 
     return (
         <View style={styles.container}>
@@ -17,16 +18,13 @@ const App = () => {
                 />
                 <Text style={styles.title}>Bem vindo(a) ao meu perfil!</Text>
                 <Text style={styles.desc}>Olá, meu nome é Vinícius, sou desenvolvedor frontend e este é meu App</Text>
-                <Link href='./tecnologias'>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Desenvolvimento</Text>
-                    </Pressable>
-                </Link>
-                <Link href='./sobre'>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Sobre mim</Text>
-                    </Pressable>
-                </Link>
+
+                <Pressable style={styles.button} onPress={() => router.push('./tecnologias')}>
+                    <Text style={styles.buttonText}>Tecnologias</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => router.push('./sobre')}>
+                    <Text style={styles.buttonText}>Sobre mim</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -35,26 +33,28 @@ const App = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
     welcome: {
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: 20,
     },
     img: {
         borderRadius: 100,
         height: 120,
-        width: 120
+        width: 120,
     },
     title: {
         fontSize: 20,
         marginTop: 10,
-        fontWeight: 500
+        fontWeight: '500',
     },
     desc: {
         fontSize: 15,
         marginTop: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+        paddingHorizontal: 20,
     },
     button: {
         backgroundColor: '#5275ff',
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#ffffff',
         textTransform: 'uppercase',
-        fontWeight: '600'
+        fontWeight: '600',
     },
 });
 
